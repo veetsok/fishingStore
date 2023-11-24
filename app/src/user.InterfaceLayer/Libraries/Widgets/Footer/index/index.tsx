26af918366e9ccb35bd6997c109f07ce18aab91c
Ivan <94634131+veetsok@users.noreply.github.com>
2023-11-24 18:07:52 +0400

    Remove ButtonLk

--- app/src/user.InterfaceLayer/Libraries/Widgets/Footer/index/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/Widgets/Footer/index/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Logo from "user.InterfaceLayer/Libraries/UI_KIT/Atoms/Logo/index/Logo";
-import ButtonLK from "user.InterfaceLayer/Libraries/UI_KIT/Atoms/ButtonLK/index";
 import Category from "user.InterfaceLayer/Libraries/UI_KIT/Molecules/Category.molecule/index/Category";
 import { useTranslation } from "react-i18next";
+import Button from "user.InterfaceLayer/Libraries/UI_KIT/Atoms/Button.atom";
+import ButtonEnum from "user.InterfaceLayer/Libraries/UI_KIT/Atoms/Button.atom/enum";
 
 import * as ST from "../styled/styled";
 
@@ -13,7 +14,7 @@ const Footer = () => {
 		<ST.footer>
 			<Logo desciption={t("navbar.logo")} />
 			<Category text={t("navbar.NavbarText", { returnObjects: true })} />
-			<ButtonLK desciption={t("footer.lk")} />
+			<Button type={ButtonEnum.enum_accountButton}>{t("footer.lk")}</Button>
 		</ST.footer>
 	);
 };
