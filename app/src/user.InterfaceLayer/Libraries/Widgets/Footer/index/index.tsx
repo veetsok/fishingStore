e240aae2a2f1baa830fabfe0c542e4d9144b4341
Ivan <94634131+veetsok@users.noreply.github.com>
2023-11-24 12:40:41 +0400

    updated

--- app/src/user.InterfaceLayer/Libraries/Widgets/Footer/index/index.tsx
+++ app/src/user.InterfaceLayer/Libraries/Widgets/Footer/index/index.tsx
@@ -4,20 +4,17 @@ import ButtonLK from "user.InterfaceLayer/Libraries/UI_KIT/Atoms/ButtonLK/index"
 import Category from "user.InterfaceLayer/Libraries/UI_KIT/Molecules/Category.molecule/index/Category";
 import { useTranslation } from "react-i18next";
 
-import * as ST from "../../../constants/container/styled";
-import * as FR from "../styled/styled";
+import * as ST from "../styled/styled";
 
 const Footer = () => {
 	const { t } = useTranslation();
 
 	return (
-		<ST.container>
-			<FR.footer>
-				<Logo desciption={t("navbar.logo")} />
-				<Category text={t("navbar.NavbarText", { returnObjects: true })} />
-				<ButtonLK desciption={t("footer.lk")} />
-			</FR.footer>
-		</ST.container>
+		<ST.footer>
+			<Logo desciption={t("navbar.logo")} />
+			<Category text={t("navbar.NavbarText", { returnObjects: true })} />
+			<ButtonLK desciption={t("footer.lk")} />
+		</ST.footer>
 	);
 };
 
--- app/src/user.InterfaceLayer/Pages/Cart.page/index.tsx
+++ app/src/user.InterfaceLayer/Pages/Cart.page/index.tsx
@@ -1,18 +1,9 @@
 import React, { FC } from "react";
-import Footer from "user.InterfaceLayer/Libraries/Widgets/Footer/index";
-import Navbar from "user.InterfaceLayer/Libraries/Widgets/Navbar/index/Navbar";
 import CartComponent from "user.InterfaceLayer/Components/CartPage.component.bll";
-import * as ST from "user.InterfaceLayer/Libraries/constants/container/styled";
 
 const LastPage: FC = () => (
 	<>
-		<ST.container>
-			<Navbar />
-			<CartComponent />
-		</ST.container>
-		<ST.footer>
-			<Footer />
-		</ST.footer>
+		<CartComponent />
 	</>
 );
 
