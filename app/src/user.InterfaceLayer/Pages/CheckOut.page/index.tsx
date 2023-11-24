465c8131556938fb13a2e657b8d7763fec5ffcb4
Ivan <94634131+veetsok@users.noreply.github.com>
2023-11-24 12:40:41 +0400

    updated

--- app/src/user.InterfaceLayer/Pages/CheckOut.page/index.tsx
+++ app/src/user.InterfaceLayer/Pages/CheckOut.page/index.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import Footer from "user.InterfaceLayer/Libraries/Widgets/Footer/index";
-import Navbar from "user.InterfaceLayer/Libraries/Widgets/Navbar/index/Navbar";
 import CheckOutComponent from "user.InterfaceLayer/Components/CheckOutPage.component.bll";
-import * as ST from "user.InterfaceLayer/Libraries/constants/container/styled";
 
 const AccountPage: React.FC = () => {
 	return (
 		<>
-			<ST.container>
-				<Navbar />
-				<ST.block>
-					<CheckOutComponent />
-				</ST.block>
-			</ST.container>
-			<ST.footer>
-				<Footer />
-			</ST.footer>
+			<CheckOutComponent />
 		</>
 	);
 };
