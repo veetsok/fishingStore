f687e083533426be30203d55df42dbc9cc09b956
Ivan <94634131+veetsok@users.noreply.github.com>
2023-11-27 23:20:12 +0400

    add Toggle Radio

---
+++ app/src/user.InterfaceLayer/Libraries/UI_KIT/Atoms/Toggle.atom/type/index.ts
@@ -0,0 +1,22 @@
+import { ICommonComponentProps } from "user.InterfaceLayer/constants/commonComponentProps";
+
+import ToggleTypeEnum from "../enum";
+
+export interface ToggleProps {
+	type: ToggleTypeEnum;
+	children?: ICommonComponentProps["children"];
+	checked?: ICommonComponentProps["checked"];
+	value?: ICommonComponentProps["value"];
+	onChange?: ICommonComponentProps["onChange"];
+	name?: ICommonComponentProps["name"];
+}
+
+export interface ToggleStyleProps {
+	margin?: ICommonComponentProps["margin"];
+	width?: ICommonComponentProps["width"];
+	height?: ICommonComponentProps["height"];
+	border?: ICommonComponentProps["border"];
+	color?: ICommonComponentProps["color"];
+	// Radio
+	borderRadio?: ICommonComponentProps["border"];
+}
